002049caaeb295b3411513e0c7ce384fef51759b
Krystian Guz <krystian.guz@outlook.com>
2023-02-01 22:38:56 +0100

    Propably Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
 import _ from 'lodash';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -9,6 +10,12 @@ const country_list = document.querySelector('.country-list');
 const country_info = document.querySelector('.country-info');
 
 function listMarkupFromData(data) {
+  /*
+  
+  function returning markup of list of countries generated from data
+
+  */
+
   let markup = '';
   data.forEach(element => {
     markup += `<li class= "country-list__item">
@@ -19,7 +26,45 @@ function listMarkupFromData(data) {
   return markup;
 }
 
+function infoMarkupFromData(data) {
+  /*
+
+  function returning markup of card of single country generated from data
+
+  */
+
+  const { name, capital, population, flags, languages } = data[0];
+  let languages_list = '';
+
+  languages.forEach((element, index) => {
+    if (index === 0) {
+      languages_list += element.name;
+    } else {
+      languages_list += `, ${element.name}`;
+    }
+  });
+
+  const markup = `
+  <h2 class="country-info__name">
+  <img src="${flags.svg}" class="country-list__flag"> ${name}
+  </h2>
+  <ul class="country-info__list">
+  <li class="country-info__list-item"><b>Capital:</b> ${capital}</li>
+  <li class="country-info__list-item"><b>Population:</b> ${population}</li>
+  <li class="country-info__list-item"><b>Languages:</b> ${languages_list}</li>
+  </ul>
+  `;
+
+  return markup;
+}
+
 const handleInput = () => {
+  /*
+  
+  function handling changes in input field
+
+  */
+
   if (input.value.length === 0) {
     return;
   }
@@ -31,12 +76,29 @@ const handleInput = () => {
       return response.json();
     })
     .then(data => {
-      if ((data.length < 10) & (data.length > 1)) {
-        console.log(data);
+      if (data.length === 1) {
+        //Displaying Info of single Country
+        country_list.style.display = 'none';
+        country_info.style.display = 'inline-block';
+
+        country_info.innerHTML = infoMarkupFromData(data);
+      } else if (data.length <= 10) {
+        //Displaying list of countries
+        country_list.style.display = 'inline-block';
+        country_info.style.display = 'none';
+
         country_list.innerHTML = listMarkupFromData(data);
+      } else {
+        //Displaying notification of too many matches
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
       }
     })
-    .catch(error => console.log(error));
+    .catch(error => {
+      console.log(error);
+      Notify.failure('Oops, there is no country with that name');
+    });
 };
 
-input.addEventListener('input', _.debounce(handleInput, 1000));
+input.addEventListener('input', _.debounce(handleInput, DEBOUNCE_DELAY));
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,11 +4,16 @@ const filtr_options = ['name', 'capital', 'population', 'flags', 'languages'];
 const start_url = 'https://restcountries.com/v2/name/';
 
 function fetchCountries(name) {
+  /*
+  
+  function returning fetch for list of countries containing string name
+
+  */
+
   const trimmed_name = name.trim();
 
   let filtr = filtr_options.toString();
 
   const url = `${start_url}${trimmed_name}?fields=${filtr}`;
-  console.log(url);
   return fetch(url);
 }
